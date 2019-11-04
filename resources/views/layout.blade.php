99b75d0dece28e30be44389280674894683db32a
Thanh Nguyen <vnscriptkid@gmail.com>
2019-11-04 08:46:05 +0700

    Include css and js to layout using mix

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,18 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <title>Hello</title>
 </head>
 <body>
-    <header style="height: 300px; background-color: blueviolet">
-        <ul>
-            <li><a href="{{ route('main') }}">App</a></li>
-            <li><a href="{{ route('home') }}">Home</a></li>
-            <li><a href="{{ route('posts.index') }}">Posts</a></li>
-            <li><a href="{{ route('posts.create') }}">Create New Post</a></li>
-            <li><a href="{{ route('about') }}">About</a></li>
-        </ul>
-    </header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="#">Blog Post</a>
+
+            <div class="collapse navbar-collapse ml-auto" id="navbarTogglerDemo03">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li class="nav-item"><a class="nav-link" href="{{ route('main') }}">App</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('home') }}">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('posts.index') }}">Posts</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('posts.create') }}">Create New Post</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('about') }}">About</a></li>
+                </ul>
+            </div>
+        </nav>
 
     {{-- status display --}}
     @if (session()->has('status'))
@@ -31,8 +39,12 @@
         </ul>
     @endif
 
-    @yield('content')
+    <div class="container">
+        @yield('content')
+    </div>
+
 
     <footer  style="height: 300px; background-color: aqua"></footer>
+    <script src="js/app.js"></script>
 </body>
 </html>
