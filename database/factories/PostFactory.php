7033fe8d3dfcc16ecaff8516c9478c7689b9bd46
Thanh Nguyen <vnscriptkid@gmail.com>
2019-11-05 14:47:58 +0700

    Add comment and post factories to seed db

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\BlogPost;
+use Faker\Generator as Faker;
+
+$factory->define(BlogPost::class, function (Faker $faker) {
+    return [
+        'title' => $faker->text(rand(10, 30)),
+        'content' => $faker->text()
+    ];
+});
