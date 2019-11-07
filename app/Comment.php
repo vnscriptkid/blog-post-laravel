c8a8bd3ac88882d88ef27431669f1a8020a10d40
Thanh Nguyen <vnscriptkid@gmail.com>
2019-11-07 18:13:31 +0700

    Add SoftDeletes to comments table

--- app/Comment.php
+++ app/Comment.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comment extends Model
 {
+    use SoftDeletes;
     // find post of a comment
     // SELECT * FROM blog_posts WHERE blog_post_id = 10
     public function blogPost()
