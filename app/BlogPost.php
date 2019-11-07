b99cbcca9654a7c8b1b04e036e61f50488c4ee2f
Thanh Nguyen <vnscriptkid@gmail.com>
2019-11-07 20:36:10 +0700

    Add relation for Post and User models

--- app/BlogPost.php
+++ app/BlogPost.php
@@ -12,10 +12,17 @@ class BlogPost extends Model
     protected $fillable = ['title', 'content', 'user_id'];
 
     // find all comments belong to one post
-    // SELECT * FROM comments WHERE blog_post_id = 10
+    // SELECT * FROM comments WHERE blog_post_id = X
     public function comments()
     {
         return $this->hasMany('App\Comment');
+    }
+
+    // find the user by whom the post is created
+    // SELECT * FROM users WHERE user_id = X LIMIT 1
+    public function user()
+    {
+        return $this->belongsTo('App\User');
     }
 
     public static function boot()
