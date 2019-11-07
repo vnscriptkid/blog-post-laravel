dafca0a761a728d0286fd44df04e1b117f5740fe
Thanh Nguyen <vnscriptkid@gmail.com>
2019-11-07 18:05:04 +0700

    Add migration to fix db config in testing env

--- database/migrations/2019_11_04_113633_create_comments_table.php
+++ database/migrations/2019_11_04_113633_create_comments_table.php
@@ -16,7 +16,13 @@ class CreateCommentsTable extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
-            $table->text('content');
+
+            if (env('DB_CONNECTION') === 'sqlite_testing') {
+                $table->text('content')->default('');
+            } else {
+                $table->text('content');
+            }
+
             $table->unsignedBigInteger('blog_post_id')->index();
             $table->foreign('blog_post_id')->references('id')->on('blog_posts');
         });
