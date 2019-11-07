45025688c15eac98e0cf9e773e698ad2063aebe8
Thanh Nguyen <vnscriptkid@gmail.com>
2019-11-07 18:07:00 +0700

    Remove auth from HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,19 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     /**
      * Show the application dashboard.
      *
@@ -23,6 +14,14 @@ class HomeController extends Controller
      */
     public function index()
     {
+        // dd(Auth::id());
+        // dd(Auth::user());
+        // dd(Auth::check());
         return view('home');
+    }
+
+    public function about()
+    {
+        return view('about');
     }
 }
