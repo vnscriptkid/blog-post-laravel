ca7a412ee69c7d797f74980fe429c06261f6bea6
Thanh Nguyen <vnscriptkid@gmail.com>
2019-11-07 18:11:01 +0700

    Add posts with SoftDeletes and model event at deleting

--- app/BlogPost.php
+++ app/BlogPost.php
@@ -3,15 +3,27 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BlogPost extends Model
 {
-    protected $fillable = ['title', 'content'];
+    use SoftDeletes;
+
+    protected $fillable = ['title', 'content', 'user_id'];
 
     // find all comments belong to one post
     // SELECT * FROM comments WHERE blog_post_id = 10
     public function comments()
     {
         return $this->hasMany('App\Comment');
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::deleting(function (BlogPost $blogPost) {
+            $blogPost->comments()->delete();
+        });
     }
 }
