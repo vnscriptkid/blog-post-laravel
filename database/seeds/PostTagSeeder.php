b311a6559292b6f12fc5830fc1b1627ad61b668b
Thanh Nguyen <vnscriptkid@gmail.com>
2019-11-09 22:55:55 +0700

    Change seeding way to random post_tag for every post

--- database/seeds/PostTagSeeder.php
+++ database/seeds/PostTagSeeder.php
@@ -13,30 +13,22 @@ class PostTagSeeder extends Seeder
      */
     public function run()
     {
-        $tags = Tag::all();
         $posts = BlogPost::all();
+        $tagsCount = Tag::count();
 
-        if ($tags->count() === 0 || $posts->count() === 0) {
+        if ($tagsCount === 0 || $posts->count() === 0) {
             $this->command->error("No post_tag record is created as no tags or posts existed");
             return;
         }
 
-        $postTagCount = (int) $this->command->ask('How many post_tag do you want to create', 200);
-
-        if ($postTagCount < 1) {
-            $postTagCount = 1;
-        }
-
         $success = 0;
 
-        for ($x = 0; $x < $postTagCount; $x++) {
-            $tag = $tags->random()->id;
-            $post = $posts->random();
-            $result = $post->tags()->syncWithoutDetaching($tag);
-            if (!empty($result['attached'])) {
-                $success++;
-            }
-        }
+        $posts->each(function ($post) use (&$success) {
+            $take = random_int(1, 5);
+            $tags = Tag::inRandomOrder()->take($take)->pluck('id');
+            $post->tags()->attach($tags);
+            $success += $take;
+        });
 
         $this->command->info("{$success} post_tag has been created successfully");
     }
