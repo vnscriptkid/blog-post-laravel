5ba65082765ca8484639907d2e85d40e81031634
Thanh Nguyen <vnscriptkid@gmail.com>
2019-11-10 20:25:54 +0700

    Add polymorph to images table and change default string length

---
+++ database/migrations/2019_11_10_112935_add_polymorph_to_images_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPolymorphToImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('images', function (Blueprint $table) {
+            $table->dropColumn('blog_post_id');
+            $table->string("imageable_type", 50);
+            $table->unsignedInteger("imageable_id");
+            $table->index(["imageable_type", "imageable_id"]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('images', function (Blueprint $table) {
+            $table->unsignedBigInteger('blog_post_id')->nullable();
+            $table->dropColumn('imageable_type');
+            $table->dropColumn('imageable_id');
+            $table->dropIndex('images_imageable_type_imageable_id_index');
+        });
+    }
+}
