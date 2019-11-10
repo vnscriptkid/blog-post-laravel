febbc0f62d8ee2b4c68192a71d873ec299660f35
Thanh Nguyen <vnscriptkid@gmail.com>
2019-11-10 10:00:53 +0700

    Reduce # of queries when fetching posts by tag and cache result

--- app/Http/Controllers/PostTagController.php
+++ app/Http/Controllers/PostTagController.php
@@ -4,13 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Tag;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class PostTagController extends Controller
 {
     public function index($tagId)
     {
         $tag = Tag::findOrFail($tagId);
-        $posts = $tag->blogPosts;
+
+        $posts = Cache::remember("posts-tag-{$tag->id}", now()->addSeconds(20), function () use ($tag) {
+            return $tag->blogPosts()
+                ->latest()
+                ->withCount('comments')
+                ->with(['user', 'tags'])
+                ->get();
+        });
 
         return view('posts.index', [
             'posts' => $posts,
