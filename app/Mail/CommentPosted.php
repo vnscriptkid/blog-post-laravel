94fda4e75f59d709a76b18e80c4d909a9fb05155
Thanh Nguyen <vnscriptkid@gmail.com>
2019-11-11 16:20:56 +0700

    Send mail to notify author someone commented on post

---
+++ app/Mail/CommentPosted.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Mail;
+
+use App\Comment;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+// default subject "Comment Posted"
+class CommentPosted extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $comment; // public vars will available in template
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Comment $comment)
+    {
+        $this->comment = $comment;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $subject = "Comment was posted on your `{$this->comment->commentable->title}` blog post";
+        return $this // ->from('admin@laravel.test', 'Admin') -> overwrite config/mail.php
+            ->subject($subject)
+            ->view('emails.posts.commented');
+    }
+}
